815a7d36a97e0e8a709482022d9576b08e3599c8
george <mullergeorgey21@gmail.com>
2025-10-30 14:25:46 +0530

    fixing

--- api/index.js
+++ api/index.js
@@ -1,30 +1,22 @@
-export default function handler(req, res) {
-  res.status(200).json({ status: 'ok' });
+import dotenv from 'dotenv';
+import connectDB from '../config/db.js';
+import app from '../app.js';
+
+dotenv.config();
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+  if (!isConnected) {
+    try {
+      await connectDB();
+      isConnected = true;
+      console.log('✅ MongoDB connected');
+    } catch (err) {
+      console.error('❌ MongoDB connection failed:', err.message);
+      return res.status(500).json({ message: 'Database connection error' });
+    }
+  }
+
+  return app(req, res);
 }
-
-
-
-
-
-// import dotenv from 'dotenv';
-// import connectDB from '../config/db.js';
-// import app from '../app.js';
-
-// dotenv.config();
-
-// let isConnected = false;
-
-// export default async function handler(req, res) {
-//   if (!isConnected) {
-//     try {
-//       await connectDB();
-//       isConnected = true;
-//       console.log('✅ MongoDB connected');
-//     } catch (err) {
-//       console.error('❌ MongoDB connection failed:', err.message);
-//       return res.status(500).json({ message: 'Database connection error' });
-//     }
-//   }
-
-//   return app(req, res);
-// }
